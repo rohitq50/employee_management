ff8492edd761efcbcde2dbce66caf51f31349394
Rohit Kumar <rohitq50@gmail.com>
2022-02-20 01:43:16 +0530

    new fixes

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+	<div id="app">
+		<Navbar />
+		<router-view @setVertical="vertical = true"/>
+		<Footer :vertical="vertical" />
+	</div>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+	<main>
+		<BasicInfo msg="Welcome to Your Vue.js App"/>
+	</main>
+</template>
+
+<script>
+// @ is an alias to /src
+import BasicInfo from '@/components/BasicInfo.vue'
+
+export default {
+	name: 'Home',
+	components: {
+		BasicInfo
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
